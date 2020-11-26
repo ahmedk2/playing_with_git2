f9c9f9b44db1cebb38bb5c44a6011c45c3d1e2d5
David Khoshnood <david.khoshnood@gmail.com>
2020-11-26 00:01:22 -0500

    Clarified Puzzle Goal (What n^2 means).

--- MorePuzzles3.py
+++ MorePuzzles3.py
@@ -2,7 +2,7 @@
 #This is a file for testing out a Puzzle in the Discrete Mathamatics Course
 
 #Puzzle To Solve: Find Perfect Square
-#Find integer n where n^2 = 31415…
+#Find Integer n where n^2 starts with 31415……
 
 #Import math
 import math
