7d4e4a66bbad1dacc12d39130743c7bd57c8be73
David Khoshnood <david.khoshnood@gmail.com>
2020-11-25 23:54:29 -0500

    Initialized base variables for solving.

--- MorePuzzles3.py
+++ MorePuzzles3.py
@@ -7,3 +7,17 @@
 #Import math
 import math
 
+# FINDING FIRST SOLUTION
+#n is number to be squared
+n = 31415
+
+#Create variable for square root of n
+x = math.sqrt(n)
+
+#N is string version of n (for comparison later)
+N = str(n)
+
+#Round square of n for turning to string later
+x = round(x,3)
+#Turn rounded square of x into integer (no decimals)
+x = int(x*1000)
