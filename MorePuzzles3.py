d624828e1b9b7bbb76592ea59113c7afb5b1e277
David Khoshnood <david.khoshnood@gmail.com>
2020-11-25 23:37:38 -0500

    Added basic outline for puzzle to solve.

--- MorePuzzles3.py
+++ MorePuzzles3.py
@@ -1,2 +1,9 @@
 
 #This is a file for testing out a Puzzle in the Discrete Mathamatics Course
+
+#Puzzle To Solve: Find Perfect Square
+#Find integer n where n^2 = 31415…
+
+#Import math
+import math
+
